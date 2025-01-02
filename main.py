519aac8d6a04afa8d34c5203573c0ba49bc69168
alexandervorobjev <chichavorobjev@gmail.com>
2025-01-02 11:24:26 +0300

    hbhb

--- main.py
+++ main.py
@@ -1,11 +1,47 @@
-fish_list = ["щука","карась","окунь","таймень","лосось","форель","хариус","кета","горбуша","кижуч"]
-spisok = []
-f = open('fish.txt','w')
-for i in fish_list:
-    print(i)
-    n = int(input("сколько раз повторить? "))
-    for k in range (0,n):
-        spisok.append(i)
-        f.write(i+'\n')
-print(spisok)
-f.close() 
+# a = list((1,2,3,4))
+# print(a)
+
+
+# x = set([1,2,3,1,3,5])
+# x.add(6)
+# print(x)
+
+
+# x = set([1,2,3,1,3,5])
+# x.add(6)
+# x.remove(5)
+# print(x)
+
+
+# x = "Hello"
+# print(x[0])
+
+
+# x = "Hello"
+# print(x[0])
+
+# print(x[-1])
+
+
+# x = "Hello"
+# print(x[0])
+# print(x[-1])
+# print(x[1:])
+
+
+# a = len("Goodbye")
+# print(a)
+
+
+# a = 8 * "x"
+# print(a)
+
+
+# a = " ".join(["Hello,","my","name","is","Sasha"])
+# print(a)
+
+
+def fun_7(a,b = 0):
+    
+
+    print(fun_7(a,b))
